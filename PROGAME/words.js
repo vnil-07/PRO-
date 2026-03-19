8bafaed33020737ad9faf02303f927eb6e3fb5cf
vnil-07 <venilsu07@gmail.com>
2026-03-19 11:36:20 +0530

    Add files via upload

---
+++ PROGAME/words.js
@@ -0,0 +1,15 @@
+const wordDatabase = {
+    animals: [
+        {word:"ELEPHANT",hint:"Big animal"},
+        {word:"TIGER",hint:"Wild cat"},
+        {word:"KANGAROO",hint:"Jumps"}
+    ],
+    fruits: [
+        {word:"APPLE",hint:"Red fruit"},
+        {word:"MANGO",hint:"King of fruits"}
+    ],
+    countries: [
+        {word:"INDIA",hint:"Taj Mahal"},
+        {word:"JAPAN",hint:"Rising sun"}
+    ]
+};
